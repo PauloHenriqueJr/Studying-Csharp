061599dc420112889309982e677fe743a97fb690
PauloHenriqueJr <paulojack2011@gmail.com>
2022-07-13 22:51:50 -0300

    Adicionar arquivos de projeto.

---
+++ Count/Program.cs
@@ -0,0 +1,16 @@
+﻿int counter = 5;  // contador = 10    
+
+while (counter > 0)
+{
+    Console.WriteLine($"Detonação em: {counter}");
+    Console.Beep(1500, 500);
+    counter--;
+    Thread.Sleep(800);
+}
+
+Console.WriteLine("Booom!\n");
+Console.Beep(800, 689);
+Thread.Sleep(1000);
+
+
+
---
+++ Majority/Program.cs
@@ -0,0 +1,20 @@
+﻿const int majority = 18;
+
+int age, years;
+
+Console.WriteLine("Escreva a sua idade");
+age = Convert.ToInt32(Console.ReadLine());
+
+// Calcula quantos anos faltam para atingir a maioridade
+
+years = majority - age;
+
+if (years > 0)
+{
+    Console.WriteLine($"Falta(m), {years} ano(s) para você atingir a maioridade\n");
+}
+else
+{
+    Console.WriteLine("Você já atingiu a maioridade\n");
+}
+
